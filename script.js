e1b80cdce71a2ede464b14495afdfc8f22f9896d
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 09:24:26 +0530

    Introduction to Objects

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 "use strict";
-const calcTip = function (bills) {
-  const x = [];
-  for (let i = 0; i < bills.length; i += 1) {
-    let tipValue =
-      300 >= bills[i] && bills[i] >= 50 ? bills[i] * 0.15 : bills[i] * 0.2;
-    x.push(tipValue);
-  }
-  return x;
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
 };
-const calcTotal = function (bills, tips) {
-  const y = [];
-  for (let i = 0; i < bills.length; i += 1) {
-    y.push(bills[i] + tips[i]);
-  }
-  return y;
-};
-const bills = [125, 555, 44];
-const tips = calcTip(bills);
-const totals = calcTotal(bills, tips);
-console.log(tips);
-console.log(totals);
