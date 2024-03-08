aa490f83191695b6621255dde85d0a4109decc2f
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 10:36:25 +0530

    Iteration: The for Loop

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 "use strict";
 
-const mark = {
-  fullName: "Mark Miller",
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
-
-const john = {
-  fullName: "John Smith",
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
-
-if (john.calcBMI() > mark.calcBMI()) {
-  console.log(
-    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`
-  );
-} else {
-  console.log(
-    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
-  );
+for (let rep = 1; rep <= 10; rep += 1) {
+  console.log(`Lifting weights repetitions ${rep} 🏋️`);
 }
