b964bd0411cd878620e14c7a47c5db396c2b4109
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 08:20:36 +0530

    Coding Exercise 5: Challenge #1

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 "use strict";
-
-function cutFruitPieces(fruit) {
-  return fruit * 5;
-}
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
-  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
-  return juice;
-}
-console.log(fruitProcessor(2, 3));
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins > 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgDolphins * 2 < avgKoalas) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log("No team wins...");
+  }
+};
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
