06b077da40b0675656023d02980306998d7ee8b6
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 12:02:10 +0530

    Challenge #4

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 "use strict";
-
-let rep = 1;
-while (rep <= 10) {
-  console.log(`Lifting weights repetition ${rep} 🏋️`);
-  rep += 1;
+const calcAverage = function (totals) {
+  let sum = 0;
+  for (let i = 0; i < totals.length; i += 1) {
+    sum += totals[i];
+  }
+  return sum / totals.length;
+};
+const calcTip = function (bills) {
+  const x = [];
+  for (let i = 0; i < bills.length; i += 1) {
+    let num =
+      300 >= bills[i] && bills[i] >= 50 ? bills[i] * 0.15 : bills[i] * 0.2;
+    x.push(num);
+  }
+  return x;
+};
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = calcTip(bills);
+const totals = [];
+for (let i = 0; i < bills.length; i += 1) {
+  totals[i] = bills[i] + tips[i];
 }
-
-let dice = Math.trunc(Math.random() * 6 + 1);
-while (dice !== 6) {
-  console.log(`Dice -> ${dice}`);
-  dice = Math.trunc(Math.random() * 6 + 1);
-  if (dice === 6) console.log("Loop is about to end...");
-}
+console.log(bills);
+console.log(tips);
+console.log(totals);
+const sum = calcAverage(totals);
+console.log(sum);
