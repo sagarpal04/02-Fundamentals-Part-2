1476f113e02d68a48a8f538930f08cf320a72a05
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 11:25:44 +0530

    Looping Backwards and Loops in Loops

--- script.js
+++ script.js
@@ -8,28 +8,11 @@ const jonas = [
   ["Michael", "Peter", "Steven"],
   true,
 ];
-const types = [];
-for (let i = 0; i < jonas.length; i += 1) {
-  console.log(jonas[i], typeof jonas[i]);
-  types.push(typeof jonas[i]);
+for (let i = jonas.length - 1; i >= 0; i -= 1) {
+  console.log(jonas[i]);
 }
-console.log(types);
-
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
-for (let i = 0; i < years.length; i += 1) {
-  ages.push(2037 - years[i]);
-}
-console.log(ages);
-
-console.log("----------- ONLY STRINGS -----------");
-for (let i = 0; i < jonas.length; i += 1) {
-  if (typeof jonas[i] != "string") continue;
-  console.log(jonas[i], typeof jonas[i]);
-}
-
-console.log("--------Working with break ---------");
-for (let i = 0; i < jonas.length; i += 1) {
-  if (typeof jonas[i] === "number") break;
-  console.log(jonas[i], typeof jonas[i]);
+for (let level = 1; level <= 5; level += 1) {
+  for (let step = 1; step <= 4; step += 1) {
+    console.log(`Level : ${level} and Step : ${step}`);
+  }
 }
