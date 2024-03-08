0c7b13c0f5fcd8b54844c83741daf97d3b0e967a
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 07:33:40 +0530

    Function Declarations vs. Expressions

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 "use strict";
 
-function logger() {
-  console.log("My name is Jonas");
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
 }
-logger();
-logger();
-logger();
+const age1 = calcAge1(1991);
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `Juice with ${apples} and ${oranges} oranges.`;
-  return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-const num = Number("23");
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
