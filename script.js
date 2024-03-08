a804468d26e5769838bfd1ab7b4e8b2d6dfd3d3b
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 07:43:34 +0530

    Arrow Functions

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict";
 
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
-const age1 = calcAge1(1991);
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
 
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
 };
-const age2 = calcAge2(1991);
-console.log(age1, age2);
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
