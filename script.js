05f6c481ac23bc12980f96fed43b2977637b0d59
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 09:51:36 +0530

    Dot vs. Bracket Notation

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const jonas = {
   job: "teacher",
   friends: ["Michael", "Peter", "Steven"],
 };
+
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const namekey = "Name";
+console.log(jonas["first" + namekey]);
+console.log(jonas["last" + namekey]);
+
+// console.log(jonas."last" + namekey);
+// const interestedIn = prompt(
+// "What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends"
+// );
+
+// if (jonas[interestedIn]) {
+// console.log(jonas[interestedIn]);
+// } else {
+// console.log(
+// "Wrong request! Choose between firstName, lastName, age, ob and friends"
+// );
+// }
+
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschmedtman";
+console.log(jonas);
+
+// Challenge
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas["friends"][0]}`
+);
